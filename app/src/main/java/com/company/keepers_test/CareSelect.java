847f929d32163ab6353e249753a7ba9e42045ac7
dohyeongHeo <dohyeong.heo@gmail.com>
2022-06-04 14:16:56 +0900

    0604
    센서 데이터 없을 경우 에러 수정

--- app/src/main/java/com/company/keepers_test/CareSelect.java
+++ app/src/main/java/com/company/keepers_test/CareSelect.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -33,7 +34,7 @@ public class CareSelect extends AppCompatActivity {
 
     // 필요한 객체 선언
     private ImageView iv_back;
-    private TextView tv_info, tv_info2, tv_info3;
+    private TextView tv_info, tv_info2, tv_info3, textView27;
     private RequestQueue requestQueue;
     private StringRequest stringRequest;
     // 마지막 활동시간을 담을 변수 lastAct
@@ -41,7 +42,7 @@ public class CareSelect extends AppCompatActivity {
     // 생활반응에 맞는 이미지를 나타낼 iv_result
     private ImageView iv_result;
     // 이미지를 담고 있는 imgArray
-    private int[] imgArray = {R.drawable.on, R.drawable.off};
+    private int[] imgArray = {R.drawable.on, R.drawable.off, R.drawable.error};
 
     k_careVO vo = new k_careVO();
     ArrayList<ValueVO> items = new ArrayList<>();
@@ -55,6 +56,7 @@ public class CareSelect extends AppCompatActivity {
         tv_info = findViewById(R.id.tv_info);
         tv_info2 = findViewById(R.id.tv_info2);
         tv_info3 = findViewById(R.id.tv_info3);
+        textView27 = findViewById(R.id.textView27);
         iv_result = findViewById(R.id.iv_result);
         iv_back = findViewById(R.id.iv_back2);
 
@@ -77,14 +79,15 @@ public class CareSelect extends AppCompatActivity {
 // RequestQueue 객체 생성
         requestQueue = Volley.newRequestQueue(this); // 현재 어플 정보 넘겨주기 -> this또는 getApplicationContext()
         //서버에 요청할 주소
-        String url = getString(R.string.KeepersIP) +"/andMonitoringAct.do";
+        String url = getString(R.string.KeepersIP) + "/andMonitoringAct.do";
 
         //stringRequest -> 요청시 필요한 문자열 객체
         stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             // 응답데이터를 받아오는 곳
             @Override
             public void onResponse(String response) {
-                Log.v("Test", response);
+
+                // Log.v("Test", response);
 
                 try {
                     JSONArray jsonArray = new JSONArray(response);
@@ -93,6 +96,7 @@ public class CareSelect extends AppCompatActivity {
                         String v_weight = jsonObject.getString("v_weight");
                         String v_signdate = jsonObject.getString("v_signdate");
                         String v_bat = jsonObject.getString("v_bat");
+                        Log.v("Test", v_bat);
                         // 스프링에서 넘어온 v_weight와 v_signdate를 ValueVo에 담기
                         ValueVO valueVO = new ValueVO(v_weight, v_signdate, v_bat);
                         // Log.v("Test", valueVO.toString());
@@ -103,15 +107,21 @@ public class CareSelect extends AppCompatActivity {
 
                     // 어레이리스트에 담긴 v_weight를 담을 배열 생성
                     Double[] result_weight = new Double[items.size()];
-                    int[] result_bat = new int[items.size()];
+                    // String[] result_bat = new String[items.size()];
+                    String lastBat = "정보 없음";
+                    String lastAct = "정보 없음";
+//                    result_bat[0] = "정보없음";
+
+
+                    // Log.v("Test", result_bat[0]);
 
 
                     for (int i = 0; i < items.size(); i++) {
                         // v_weight 담기
                         result_weight[i] = Double.parseDouble(items.get(i).getV_weight());
-                        result_bat[i] = Integer.parseInt(items.get(i).getV_bat());
-                        // Log.v("Test", items.get(i).getV_weight() + "" + i);
-
+                        if (i == 0) {
+                            lastBat = items.get(i).getV_bat()+"%";
+                        }
                         // 무게 데이터 값이 10보다 클 경우
                         // 해당 시간 값을 lastAct에 담기
                         if (Double.parseDouble(items.get(i).getV_weight()) > 10) {
@@ -120,29 +130,42 @@ public class CareSelect extends AppCompatActivity {
                         }
                     }
 
-                    String lastBat = String.valueOf(result_bat[0]);
+                    // String lastBat = result_bat[0];
+
 
                     // 무게 데이터 최신 값이 10보다 클 경우
                     // 활동중으로 텍스트 변경, 이미지 변경
-                    if (result_weight[0] > 10) {
-                        tv_info2.setText("활동중");
-                        iv_result.setImageResource(imgArray[0]);
+                    try {
+                        if (result_weight[0] > 10) {
+                            tv_info2.setText("활동중");
+                            iv_result.setImageResource(imgArray[0]);
+                            // 무게 데이터 최신 값이 10보다 작을 경우
+                            // 활동중으로 텍스트 변경, 이미지 변경
+                        } else {
+                            tv_info2.setText("무반응");
+                            iv_result.setImageResource(imgArray[1]);
+                        }
 
 
-                        // 무게 데이터 최신 값이 10보다 작을 경우
-                        // 활동중으로 텍스트 변경, 이미지 변경
-                    } else {
-                        tv_info2.setText("무반응");
-                        iv_result.setImageResource(imgArray[1]);
+                        // Log.v("Test", lastAct);
+
+                        // result_bat 값으로 텍스트 변경
+                        tv_info3.setText(lastBat);
+
+                        // lastAct 값으로 텍스트 변경
+                        tv_info.setText(lastAct);
+
+
+                    } catch (Exception e) {
+                        tv_info.setText("데이터가 없습니다.");
+                        tv_info2.setText("데이터가 없습니다.");
+                        textView27.setText("");
+                        // tv_info3.setText("데이터가 없습니다.");
+                        iv_result.setImageResource(imgArray[2]);
+                        // Toast.makeText(CareSelect.this, "데이터가 없습니다.", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
                     }
 
-                    // Log.v("Test", lastAct);
-
-                    // result_bat 값으로 텍스트 변경
-                    tv_info3.setText(lastBat+"%");
-
-                    // lastAct 값으로 텍스트 변경
-                    tv_info.setText(lastAct.toString());
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -150,6 +173,8 @@ public class CareSelect extends AppCompatActivity {
 
 
             }
+
+
         }, new Response.ErrorListener() {
             // 서버와의 연동 에러시 출력
             @Override
